9d1f511beb82d0a29954b7cd9611f75bf7bb3a6f
최지훈 <cjh0630@naver.com>
2022-09-17 03:01:11 +0900

    aaa

--- LeetCode_/src/leet_338.java
+++ LeetCode_/src/leet_338.java
@@ -1,4 +1,10 @@
-
-public class leet_338 {
-
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n+1];
+        
+        for (int i = 0; i <= n; i++)
+            result[i] = Integer.bitCount(i);
+        
+        return result;
+    }
 }
