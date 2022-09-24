7b8455746a6114764d33c1d5854945419e4baea1
최지훈 <cjh0630@naver.com>
2022-09-24 14:09:44 +0900

    에러가 발생한다.. 코드는 맞는거 같은데

--- LeetCode_/src/leet_397.java
+++ LeetCode_/src/leet_397.java
@@ -1,4 +1,32 @@
-
-public class leet_397 {
-
+class Solution {
+    public int integerReplacement(int n) {
+        int []memo = new int[n + 2];
+        for(int i = 0; i < n + 2; i++)
+            memo[i] = -1;
+        return find_best(n,memo);
+    }
+    int find_best(int n, int []memo){
+        int result;
+        if(memo[n]!=-1){
+            result = memo[n];
+            return memo[n];
+        }
+        if(n <= 1){
+            result =  0;
+            memo[n] = result;
+        }
+        else{
+            if(n % 2 == 0){
+                result = 1 + find_best(n/2,memo);
+                memo[n] = result;
+            }
+            else{
+                result = 1 + Math.min(find_best(n-1,memo),find_best(n+1,memo));
+                memo[n] = result;
+            }
+        }
+        result = memo[n];
+        return result;
+    }
+    
 }
