9e78a2f4f442e5906f51c5da72e8d11b3691ebf3
최지훈 <cjh0630@naver.com>
2022-10-03 15:59:48 +0900

    commit

--- LeetCode_/src/leet_1014.java
+++ LeetCode_/src/leet_1014.java
@@ -1,4 +1,21 @@
-
-public class leet_1014 {
-
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int max = Integer.MIN_VALUE;
+        int[] dp = new int [values.length];
+        dp[0] = values[0];
+        for(int i = 1; i< values.length;i++){
+            max = Math.max(max,dp[i-1]+values[i]-i);
+            dp[i] = Math.max(dp[i-1],values[i] + i);
+        }
+        return max; //참고한 코드
+        /*int max = Integer.MIN_VALUE;
+        for(int i = 0; i < values.length;i++){
+            for(int j=i+1;j<values.length;j++){
+                int second = values[i] + values[j] + i - j;
+                max = Math.max(max,second);
+            }
+        }
+        return max; 오류가 발생한 코드*/
+    }
+    
 }
