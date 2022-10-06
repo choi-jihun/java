d9d949a1af7d0c33b968692c0cdc832f0cae31e0
최지훈 <cjh0630@naver.com>
2022-10-06 20:08:15 +0900

    leet_code

--- LeetCode_/src/leet_207.java
+++ LeetCode_/src/leet_207.java
@@ -1,4 +1,41 @@
+class Solution {
+    static int[] visited;
+    static int[] check;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        visited = new int[numCourses+1];
+        check = new int[numCourses+1];
+        int[][] arr = new int[numCourses+1][numCourses+1];
+        for(int i=0;i<prerequisites.length;i++){
+               arr[prerequisites[i][0]][prerequisites[i][1]]=1;
+        }
+        for(int i=0;i<numCourses+1;i++){
+     
+            if(check[i] != 1 &&has_cycle(i,arr, numCourses+1)){
+                return false;
+            }
+        }
+        return true;
+    }
 
-public class leet_207 {
+    public boolean has_cycle(int n, int arr[][], int len) {
+      int i;
+      int cycle = 0;
+        
+      if (visited[n] == 0) {
+            if(check[n]==1)
+                return false;
+         visited[n] = 1;
+            check[n] = 1;
+         for (i = 0; i < len; i++)
+            if (arr[n][i] == 1)
+               if (has_cycle(i,arr,len))
+                  return true;
+         visited[n] = 0;
+           
+         return false;
+      } else
+         return true;
+   }
+
 
 }
--- LeetCode_/src/leet_997.java
+++ LeetCode_/src/leet_997.java
@@ -1,4 +1,22 @@
-
-public class leet_997 {
-
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int ans = -1;
+        if(n <= 1)
+            return 1;
+        int []arr = new int [n+1];
+        for(int i = 0; i < trust.length; i++){
+            arr[trust[i][1]]++;
+            if(arr[trust[i][1]] == n -1){
+                ans=trust[i][1];
+                break;
+            }
+        }
+        for(int i=0; i< trust.length;i++){
+            if(trust[i][0] == ans){
+                ans = -1;
+                break;
+            }
+        }
+        return ans;
+    }
 }
