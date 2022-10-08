903eebcf2d2b5c8c4b1de3e7cd5aa294a94a0c93
최지훈 <cjh0630@naver.com>
2022-10-08 21:03:30 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_2839.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class baekjoon_2839 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int N = input.nextInt();
+		int count = 0;
+		
+		while(true) {
+			if(N%5 ==0) {
+				System.out.println(N/5+count);
+				break;
+			}
+			else if(N<0) {
+				System.out.println(-1);
+				break;
+			}
+			count++;
+			N-=3;
+				
+		}
+	}
+
+}
