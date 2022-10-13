967bcd5fe467280a64b7619d78816078cf7c7de1
최지훈 <cjh0630@naver.com>
2022-10-13 20:59:28 +0900

    programmers

--- LeetCode_/src/programmers_타겟넘버.java
+++ LeetCode_/src/programmers_타겟넘버.java
@@ -1,4 +1,23 @@
-
-public class programmers_타겟넘버 {
-
+class Solution {
+    static int answer = 0;
+    public int solution(int[] numbers, int target) {
+        
+        
+        return DFS(numbers,target,0,0);
+    }
+    static int DFS(int []numbers, int target,int depth, int sum){
+        if(depth == numbers.length){
+            if(target == sum){
+                answer++;
+                return answer;
+            }
+                
+        }
+        else{
+            DFS(numbers,target,depth+1,sum+(numbers[depth]));
+            DFS(numbers,target,depth+1,sum-(numbers[depth]));
+        }
+        
+        return answer;
+    }
 }
