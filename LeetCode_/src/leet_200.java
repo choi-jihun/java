384b6c5521fd7f77bb03264ce9d15fadf02fb68a
최지훈 <cjh0630@naver.com>
2022-10-13 20:58:01 +0900

    leetcode

--- LeetCode_/src/leet_200.java
+++ LeetCode_/src/leet_200.java
@@ -1,4 +1,25 @@
-
-public class leet_200 {
-
+class Solution {
+    static boolean[][] visited;
+    public int numIslands(char[][] grid) {
+        visited = new boolean [grid.length][grid[0].length];
+        int cnt = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    dfs(i,j,grid);
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+    static void dfs(int x, int y, char[][] grid){
+        if(x<0||y<0||x==grid.length||y==grid[0].length || grid[x][y] == '0' || visited[x][y])
+            return;
+        visited[x][y]=true;
+        dfs(x+1,y,grid);
+        dfs(x-1,y,grid);
+        dfs(x,y+1,grid);
+        dfs(x,y-1,grid);
+    }
 }
--- LeetCode_/src/leetcode_695.java
+++ LeetCode_/src/leetcode_695.java
@@ -1,4 +1,27 @@
-
-public class leetcode_695 {
-
+class Solution {
+    static boolean [][] visited;
+    public int maxAreaOfIsland(int[][] grid) {
+        visited = new boolean [grid.length][grid[0].length];
+        int ans = Integer.MIN_VALUE;
+        for(int  i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(!visited[i][j]){
+                    int size = DFS(i,j,grid);
+                    ans = Math.max(size,ans);
+                }
+            }
+        }
+        return ans;
+    }
+    static int DFS(int x, int y, int [][]grid){
+        if(x<0||y<0||x>=grid.length||y>=grid[0].length||grid[x][y] == 0||visited[x][y]==true)
+            return 0;
+        visited[x][y] = true;
+        int size = 1;
+        size += DFS(x+1,y,grid);
+        size += DFS(x,y+1,grid);
+        size += DFS(x-1,y,grid);
+        size += DFS(x,y-1,grid);
+        return size;
+    }
 }
