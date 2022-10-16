c370d099b343e34a8d792dbd8ac78a6c5527d4f6
최지훈 <cjh0630@naver.com>
2022-10-16 16:49:58 +0900

    programmers

--- LeetCode_/src/programmers_네트워크.java
+++ LeetCode_/src/programmers_네트워크.java
@@ -1,4 +1,21 @@
-
-public class programmers_네트워크 {
-
+class Solution {
+    static boolean visited[];
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean [n];
+        for(int i =0; i<n;i++){
+            if(!visited[i]){
+                dfs(computers,i);
+                answer++;
+            }
+        }
+        return answer;
+    }
+    static void dfs(int [][]computers, int start){
+        visited[start] = true;
+        for(int j = 0; j < computers.length; j++){
+            if(!visited[j] && computers[start][j] == 1)
+                dfs(computers,j);
+        }
+    }
 }
