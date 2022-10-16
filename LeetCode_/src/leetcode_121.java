5f923b5ddd98e0b8759706d8c42bfa08cbfba209
최지훈 <cjh0630@naver.com>
2022-10-16 15:50:05 +0900

    leetcode

--- LeetCode_/src/leetcode_121.java
+++ LeetCode_/src/leetcode_121.java
@@ -1,4 +1,13 @@
-
-public class leetcode_121 {
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length <= 1)
+            return 0;
+        int best = 0;
+        int min = prices[0];
+        for(int  i =0; i < prices.length; i++){
+            min = Math.min(prices[i],min);
+            best = Math.max(prices[i] - min, best);
+        }
+        return best;
+    }
 }
