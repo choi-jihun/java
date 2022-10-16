53e5f4f99e3b26b6576d6e70505b11a24e38ca3b
최지훈 <cjh0630@naver.com>
2022-10-16 15:17:58 +0900

    commit

--- LeetCode_/src/leetcode_70.java
+++ LeetCode_/src/leetcode_70.java
@@ -1,4 +1,13 @@
-
-public class leetcode_70 {
-
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2)
+            return n;
+        int []dp = new int [n+2];
+        dp[1] = 1;
+        dp[2] = 2;    
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
 }
