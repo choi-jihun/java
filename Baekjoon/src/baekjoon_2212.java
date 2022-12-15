49f39c1236b044979441e800d476d3ab551373e0
최지훈 <cjh0630@naver.com>
2022-12-15 18:53:04 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_2212.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baekjoon_2212 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int []arr = new int [N];
+        for(int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        int []cost = new int [N-1];
+        for(int  i = 0; i <N-1; i++)
+            cost[i] = arr[i+1] - arr[i];
+        Arrays.sort(cost);
+        int sum = 0;
+        for(int i = 0; i < N - K; i++)
+            sum += cost[i];
+        System.out.println(sum);
+    }
+}
