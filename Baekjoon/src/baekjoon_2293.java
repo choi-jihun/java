a3a35539272654fe897da1bd96d7934cd7cc6950
최지훈 <cjh0630@naver.com>
2023-02-04 19:41:43 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_2293.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_2293 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int []coin = new int [n];
+        int []dp = new int [k+1];
+
+        for(int i = 0; i < n; i++)
+            coin[i] = Integer.parseInt(br.readLine());
+
+        dp[0] = 1;
+        for(int i = 0; i < n; i++)
+            for(int j = 1; j < k + 1; j++)
+                if(j >= coin[i])
+                    dp[j] += dp[j-coin[i]];
+
+        System.out.println(dp[k]);
+    }
+}
