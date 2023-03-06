6d0ebdcaa63132a88272e3e008dd5451fca67394
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-03-06 12:10:26 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_17413.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class baekjoon_17413 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        Stack <Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '<'){
+                while (!stack.isEmpty())
+                    sb.append(stack.pop());
+                flag = true;
+            }
+            if(str.charAt(i) == '>'){
+                flag = false;
+                sb.append(str.charAt(i));
+                continue;
+            }
+            if(flag == true)
+                sb.append(str.charAt(i));
+            if(flag == false){
+                if(str.charAt(i)==' '){
+                    while (!stack.isEmpty())
+                        sb.append(stack.pop());
+                    sb.append(' ');
+                    continue;
+                }
+                else
+                    stack.push(str.charAt(i));
+            }
+            if(i == str.length()-1)
+                while (!stack.isEmpty())
+                    sb.append(stack.pop());
+        }
+        System.out.println(sb);
+    }
+}
