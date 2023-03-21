95ce935190a857504dafb08ab5a223dd8fbc883b
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-03-21 20:08:00 +0900

    baekjoon

--- Baekjoon/src/baekjoon_1032.java
+++ Baekjoon/src/baekjoon_1032.java
@@ -1,4 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class baekjoon_1032 {
+
+   public static void main(String[] args) throws NumberFormatException, IOException {
+      // TODO Auto-generated method stub
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int N = Integer.parseInt(br.readLine());
+      
+      char []pattern = br.readLine().toCharArray();
+      
+      for (int i = 0; i < N - 1; i++) {
+         char[] fileName = br.readLine().toCharArray();
+         for (int j = 0; j < pattern.length; j++) {
+            if(pattern[j] != fileName[j]) pattern[j] = '?';
+            }
+         }
+      System.out.println(pattern);
+   }
 
 }
