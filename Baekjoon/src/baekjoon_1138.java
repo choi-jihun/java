5f4c41042e14430ebb77456e313c86087c217987
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-04-09 13:44:55 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_1138.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_1138 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 1; i < N + 1; i++) {
+            int cnt = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                if (cnt == 0 && arr[j] == 0) {
+                    arr[j] = i;
+                    break;
+                } else if (arr[j] == 0)
+                    cnt--;
+            }
+        }
+        for (int i = 0; i < N; i++)
+            System.out.print(arr[i] + " ");
+
+    }
+}
