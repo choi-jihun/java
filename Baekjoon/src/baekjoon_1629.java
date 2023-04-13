4bb07c12ac75340a17697ba20501328e3ce1e4fe
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-04-13 18:36:09 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_1629.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_1629 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+        long C = Integer.parseInt(st.nextToken());
+
+        System.out.println(pow(A, B, C));
+    }
+
+    static long pow(long A, long B, long C) {
+        if (B == 1)
+            return A % C;
+
+        long temp = pow(A, B / 2, C);
+
+        if (B % 2 == 1)
+            return (temp * temp) % C * A % C;
+        return temp * temp % C;
+    }
+}
