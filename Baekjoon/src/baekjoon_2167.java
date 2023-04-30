2fecce024b701d66d7911e7762236a5739749ef4
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-04-30 12:51:18 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_2167.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_2167 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++)
+                arr[i][j] = Integer.parseInt(st.nextToken());
+        }
+        int K = Integer.parseInt(br.readLine());
+        for (int t = 0; t < K; t++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken()) - 1;
+            int j = Integer.parseInt(st.nextToken()) - 1;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+
+            int sum = 0;
+            for (int a = i; a <= x; a++) {
+                for (int b = j; b <= y; b++)
+                    sum += arr[a][b];
+            }
+            sb.append(sum).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
+
