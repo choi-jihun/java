0a98cd186e48667858f5fb58559faad82a0250e4
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-05-16 19:30:46 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_2775.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon_2775 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[][] dp = new int[15][15];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 15; i++) {
+            dp[i][1] = 1;
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < 15; i++) {
+            for (int j = 2; j < 15; j++)
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+        }
+
+        for (int i = 0; i < T; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[k][n]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
