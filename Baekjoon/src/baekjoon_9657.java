61f27bb8cbfcd4bfc1174ba15fe928a82e310f7e
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-07-08 13:07:04 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_9657.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon_9657 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N % 7 == 0 || N % 7 == 2)
+            System.out.println("CY");
+        else
+            System.out.println("SK");
+    }
+}
