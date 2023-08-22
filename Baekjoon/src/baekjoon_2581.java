f55598410073844d083b9be79c17723a8a8e006c
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-08-22 13:00:47 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_2581.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekjoon_2581 {
+
+    public static boolean prime[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+
+        prime = new boolean[N + 1];
+        get_prime();
+
+        for (int i = M; i <= N; i++) {
+            if (!prime[i]) {
+                sum += i;
+                if (min == Integer.MAX_VALUE)
+                    min = i;
+            }
+        }
+        if (sum == 0)
+            System.out.println(-1);
+        else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+
+    public static void get_prime() {
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+
+    }
+}
