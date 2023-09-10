253a0830609a7ceadca669b38436c74e4d48c7a3
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-09-10 13:48:49 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_2605.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class baekjoon_2605 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num == 0)
+                arr.add(i + 1);
+            else
+                arr.add(arr.size() - num, i + 1);
+        }
+
+
+        for (int i = 0; i < N; i++)
+            System.out.print(arr.get(i) + " ");
+    }
+}
