ef01d553f9adea8236c342d9a9a166ce44ec2f9c
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-12-20 11:01:12 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_2204.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class baekjoon_2204 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            String temp = br.readLine();
+            if (temp.equals("0")) {
+                break;
+            }
+
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < Integer.parseInt(temp); i++) {
+                list.add(br.readLine());
+            }
+            list.sort(String.CASE_INSENSITIVE_ORDER);
+            sb.append(list.get(0)).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
