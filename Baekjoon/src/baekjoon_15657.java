5f7acd8c473214c5749a817efc9da993fa4b1d83
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2024-02-25 16:02:22 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_15657.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baekjoon_15657 {
+    static int N, M;
+    static int[] arr;
+    static int[] select;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+        select = new int[M];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0, 0);
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    static void dfs(int idx, int depth) {
+        if (depth == M) {
+            for (int num : select) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = idx; i < N; i++) {
+            select[depth] = arr[i];
+            dfs(i, depth + 1);
+        }
+
+    }
+}
