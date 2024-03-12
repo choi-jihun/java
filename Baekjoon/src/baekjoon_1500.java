0f001f38ab87e8e24760a04643c1ee92bb96e554
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2024-03-12 18:48:47 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_1500.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class baekjoon_1500 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int div = S / K;
+        int mod = S % K;
+        long max = 1;
+        for (int i=1; i<=K; i++) {
+            if(i<=mod) {
+                max *= (div+1);
+            }else {
+                max *= div;
+            }
+        }
+        bw.write(String.valueOf(max));
+        bw.flush();
+    }
+}
