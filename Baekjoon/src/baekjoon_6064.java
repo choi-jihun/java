ce1375e05e363bb5220f4b9208ca7ee2bfb44f49
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2024-04-04 22:39:24 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_6064.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class baekjoon_6064 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            int result = -1;
+
+            for (int k = x; k <= lcm(M, N); k += M) {
+                if ((k - y) % N == 0) {
+                    result = k;
+                    break;
+                }
+            }
+            bw.write(result + "\n");
+        }
+        bw.flush();
+
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
