8545a0ff7e474d93e47b865dcd87f45068c6943b
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2024-05-08 19:00:00 +0900

    baekjoon

---
+++ Baekjoon/src/baekjoon_18353.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baekjoon_18353 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+        int max = 1;
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        bw.write(String.valueOf(N - max));
+        bw.flush();
+    }
+
+}
